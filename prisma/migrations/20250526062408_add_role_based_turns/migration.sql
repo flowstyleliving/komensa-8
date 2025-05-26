64ee49079c1acf588d23f885b2af6e25bd41e327
flowstyleliving <flowstyleliving@gmail.com>
2025-05-26 06:10:28 -0700

    gitignore

--- prisma/migrations/20250526062408_add_role_based_turns/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "ChatTurnState" ADD COLUMN     "current_turn_index" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "next_role" TEXT,
-ADD COLUMN     "turn_queue" JSONB NOT NULL DEFAULT '[]';
