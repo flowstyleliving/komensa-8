305b99880b0cea3905ce7d7e9e63f80116ae82a4
flowstyleliving <flowstyleliving@gmail.com>
2025-05-29 22:28:33 -0700

    wrapped in suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import "./globals.css"
 import { Providers } from '@/lib/providers'
+import { Suspense } from "react"
 
 export const metadata: Metadata = {
   title: {
@@ -80,6 +81,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
@@ -99,5 +101,6 @@ export default function RootLayout({
         </div>
       </body>
     </html>
+    </Suspense>
   )
 }
